27ebf02228e587d99b9ff2c3b55b55abbdf717d1
haruyasu <harukun2002@gmail.com>
2023-12-17 10:03:28 +0900

    リファクタリング

--- frontend/actions/user.ts
+++ frontend/actions/user.ts
@@ -1,5 +1,35 @@
 "use server"
 
+// 共通のAPIリクエスト
+const fetchAPI = async (url: string, options: RequestInit) => {
+  const apiUrl = process.env.API_URL
+
+  if (!apiUrl) {
+    return { success: false, error: "API URLが設定されていません" }
+  }
+
+  try {
+    const response = await fetch(`${apiUrl}${url}`, options)
+
+    if (!response.ok) {
+      return { success: false, error: "APIでエラーが発生しました" }
+    }
+
+    // Content-Type ヘッダーが application/json の場合のみ、JSON を解析する
+    const contentType = response.headers.get("Content-Type")
+    if (contentType && contentType.includes("application/json")) {
+      const data = await response.json()
+      return { success: true, data }
+    }
+
+    // データなしで成功を返す
+    return { success: true }
+  } catch (error) {
+    console.error(error)
+    return { success: false, error: "ネットワークエラーが発生しました" }
+  }
+}
+
 interface TemporarrySignupProps {
   name: string
   email: string
@@ -14,41 +44,30 @@ export const temporarrySignup = async ({
   password,
   rePassword,
 }: TemporarrySignupProps) => {
-  try {
-    const body = JSON.stringify({
-      name,
-      email,
-      password,
-      re_password: rePassword,
-    })
+  const body = JSON.stringify({
+    name,
+    email,
+    password,
+    re_password: rePassword,
+  })
 
-    // アカウント仮登録を送信
-    const apiRes = await fetch(`${process.env.API_URL}/api/auth/users/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body,
-    })
-
-    // APIレスポンスが正常でない場合、失敗を返す
-    if (!apiRes.ok) {
-      return {
-        success: false,
-      }
-    }
-
-    // 成功を返す
-    return {
-      success: true,
-    }
-  } catch (error) {
-    console.error(error)
-    // エラー発生時に、失敗を返す
-    return {
-      success: false,
-    }
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
   }
+
+  // アカウント仮登録を送信
+  const result = await fetchAPI("/api/auth/users/", options)
+
+  if (!result.success) {
+    console.error(result.error)
+    return { success: false }
+  }
+
+  return { success: true }
 }
 
 interface CompleteSignupProps {
@@ -58,42 +77,28 @@ interface CompleteSignupProps {
 
 // アカウント本登録
 export const completeSignup = async ({ uid, token }: CompleteSignupProps) => {
-  try {
-    const body = JSON.stringify({
-      uid,
-      token,
-    })
+  const body = JSON.stringify({
+    uid,
+    token,
+  })
 
-    // アカウント本登録を送信
-    const apiRes = await fetch(
-      `${process.env.API_URL}/api/auth/users/activation/`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body,
-      }
-    )
-
-    // APIレスポンスが正常でない場合、失敗を返す
-    if (!apiRes.ok) {
-      return {
-        success: false,
-      }
-    }
-
-    // 成功を返す
-    return {
-      success: true,
-    }
-  } catch (error) {
-    console.error(error)
-    // エラー発生時に、失敗を返す
-    return {
-      success: false,
-    }
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
   }
+
+  // アカウント本登録を送信
+  const result = await fetchAPI("/api/auth/users/activation/", options)
+
+  if (!result.success) {
+    console.error(result.error)
+    return { success: false }
+  }
+
+  return { success: true }
 }
 
 interface ForgotPasswordProps {
@@ -102,41 +107,27 @@ interface ForgotPasswordProps {
 
 // パスワード再設定
 export const forgotPassword = async ({ email }: ForgotPasswordProps) => {
-  try {
-    const body = JSON.stringify({
-      email,
-    })
+  const body = JSON.stringify({
+    email,
+  })
 
-    // パスワード再設定を送信
-    const apiRes = await fetch(
-      `${process.env.API_URL}/api/auth/users/reset_password/`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body,
-      }
-    )
-
-    // APIレスポンスが正常でない場合、失敗を返す
-    if (!apiRes.ok) {
-      return {
-        success: false,
-      }
-    }
-
-    // 成功を返す
-    return {
-      success: true,
-    }
-  } catch (error) {
-    console.error(error)
-    // エラー発生時に、失敗を返す
-    return {
-      success: false,
-    }
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
   }
+
+  // パスワード再設定を送信
+  const result = await fetchAPI("/api/auth/users/reset_password/", options)
+
+  if (!result.success) {
+    console.error(result.error)
+    return { success: false }
+  }
+
+  return { success: true }
 }
 
 interface ResetPasswordProps {
@@ -153,44 +144,33 @@ export const resetPassword = async ({
   newPassword,
   reNewPassword,
 }: ResetPasswordProps) => {
-  try {
-    const body = JSON.stringify({
-      uid,
-      token,
-      new_password: newPassword,
-      re_new_password: reNewPassword,
-    })
+  const body = JSON.stringify({
+    uid,
+    token,
+    new_password: newPassword,
+    re_new_password: reNewPassword,
+  })
 
-    // パスワード再設定確認を送信
-    const apiRes = await fetch(
-      `${process.env.API_URL}/api/auth/users/reset_password_confirm/`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body,
-      }
-    )
-
-    // APIレスポンスが正常でない場合、失敗を返す
-    if (!apiRes.ok) {
-      return {
-        success: false,
-      }
-    }
-
-    // 成功を返す
-    return {
-      success: true,
-    }
-  } catch (error) {
-    console.error(error)
-    // エラー発生時に、失敗を返す
-    return {
-      success: false,
-    }
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
   }
+
+  // パスワード再設定確認を送信
+  const result = await fetchAPI(
+    "/api/auth/users/reset_password_confirm/",
+    options
+  )
+
+  if (!result.success) {
+    console.error(result.error)
+    return { success: false }
+  }
+
+  return { success: true }
 }
 
 export interface UserDetailType {
@@ -208,37 +188,22 @@ interface GetUserDetailProps {
 
 // ユーザー詳細取得
 export const getUserDetail = async ({ userId }: GetUserDetailProps) => {
-  try {
-    // APIからユーザー詳細を取得
-    const apiRes = await fetch(`${process.env.API_URL}/api/users/${userId}/`, {
-      method: "GET",
-      cache: "no-store",
-    })
-
-    // APIレスポンスが正常でない場合、失敗とnullを返す
-    if (!apiRes.ok) {
-      return {
-        success: false,
-        user: null,
-      }
-    }
-
-    // レスポンスをJSONとして解析し、ユーザー詳細を取得
-    const user: UserDetailType = await apiRes.json()
-
-    // 成功と取得したユーザー詳細を返す
-    return {
-      success: true,
-      user,
-    }
-  } catch (error) {
-    console.error(error)
-    // エラー発生時に、失敗とnullを返す
-    return {
-      success: false,
-      user: null,
-    }
+  const options: RequestInit = {
+    method: "GET",
+    cache: "no-store",
   }
+
+  // APIからユーザー詳細を取得
+  const result = await fetchAPI(`/api/users/${userId}/`, options)
+
+  if (!result.success) {
+    console.error(result.error)
+    return { success: false, user: null }
+  }
+
+  const user: UserDetailType = result.data
+
+  return { success: true, user }
 }
 
 interface UpdateUserProps {
@@ -255,47 +220,32 @@ export const updateUser = async ({
   introduction,
   avatar,
 }: UpdateUserProps) => {
-  try {
-    const body = JSON.stringify({
-      name,
-      introduction,
-      avatar,
-    })
+  const body = JSON.stringify({
+    name,
+    introduction,
+    avatar,
+  })
 
-    // プロフィール編集を送信
-    const apiRes = await fetch(`${process.env.API_URL}/api/auth/users/me/`, {
-      method: "PATCH",
-      headers: {
-        Authorization: `JWT ${accessToken}`,
-        "Content-Type": "application/json",
-      },
-      body,
-    })
-
-    // APIレスポンスが正常でない場合、失敗とnullを返す
-    if (!apiRes.ok) {
-      return {
-        success: false,
-        user: null,
-      }
-    }
-
-    // レスポンスをJSONとして解析し、ユーザー詳細を取得
-    const user: UserDetailType = await apiRes.json()
-
-    // 成功と取得したユーザー詳細を返す
-    return {
-      success: true,
-      user,
-    }
-  } catch (error) {
-    console.error(error)
-    // エラー発生時に、失敗とnullを返す
-    return {
-      success: false,
-      user: null,
-    }
+  const options = {
+    method: "PATCH",
+    headers: {
+      Authorization: `JWT ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+    body,
   }
+
+  // プロフィール編集を送信
+  const result = await fetchAPI("/api/auth/users/me/", options)
+
+  if (!result.success) {
+    console.error(result.error)
+    return { success: false, user: null }
+  }
+
+  const user: UserDetailType = result.data
+
+  return { success: true, user }
 }
 
 interface UpdatePasswordProps {
@@ -312,42 +262,28 @@ export const updatePassword = async ({
   newPassword,
   reNewPassword,
 }: UpdatePasswordProps) => {
-  try {
-    const body = JSON.stringify({
-      current_password: currentPassword,
-      new_password: newPassword,
-      re_new_password: reNewPassword,
-    })
+  const body = JSON.stringify({
+    current_password: currentPassword,
+    new_password: newPassword,
+    re_new_password: reNewPassword,
+  })
 
-    // パスワード変更を送信
-    const apiRes = await fetch(
-      `${process.env.API_URL}/api/auth/users/set_password/`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `JWT ${accessToken}`,
-          "Content-Type": "application/json",
-        },
-        body,
-      }
-    )
-
-    // APIレスポンスが正常でない場合、失敗を返す
-    if (!apiRes.ok) {
-      return {
-        success: false,
-      }
-    }
-
-    // 成功を返す
-    return {
-      success: true,
-    }
-  } catch (error) {
-    console.error(error)
-    // エラー発生時に、失敗を返す
-    return {
-      success: false,
-    }
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `JWT ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+    body,
   }
+
+  // パスワード変更を送信
+  const result = await fetchAPI("/api/auth/users/set_password/", options)
+
+  if (!result.success) {
+    console.error(result.error)
+    return { success: false }
+  }
+
+  return { success: true }
 }
